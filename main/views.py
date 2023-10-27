f74399ac54d51eec8d09969ea678b9d9a1b7c423
rafaeldewandaru <rafael29204@gmail.com>
2023-10-27 13:16:30 +0700

    progress landing page admin and user

--- main/views.py
+++ main/views.py
@@ -1,13 +1,54 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.core import serializers
+from django.views.decorators.csrf import csrf_exempt
 from account.models import User
+from book.models import Book
 
 @login_required(login_url='account/login')
 def show_main(request):
     if request.user.role == User.ADMIN:
-        return render(request, "landing_page_admin.html")
+        products = Book.objects.all()
+        context = {
+            'role': 'Admin',
+            'name': request.user.username,
+            'products': products,
+        }
+        return render(request, "landing_page_admin.html", context)
     
     elif request.user.role == User.USER:
-        return render(request, "landing_page_user.html")
+        products = Book.objects.filter(is_borrowed=False)
+        context = {
+            'role': 'User',
+            'name': request.user.username,
+            'products': products,
+        }
+        return render(request, "landing_page_user.html", context)
+    
 
-# Create your views here.
+@csrf_exempt
+def add_book_ajax(request):
+    if request.method == 'POST':
+        title = request.POST.get("title")
+        authors = request.POST.get("authors")
+        categories = request.POST.get("categories")
+        thumbnail =  request.POST.get("thumbnail")
+        description = request.POST.get("description")
+        published_year = request.POST.get("published_year")
+        
+        new_product = Book(title=title, authors=authors, categories=categories, thumbnail=thumbnail, description=description, published_year=published_year)
+        new_product.save()
+
+        return HttpResponse(b"CREATED", status=201)
+    return HttpResponseNotFound()
+
+
+def get_books_json(request):
+    if request.user.role == User.ADMIN:
+        books = Book.objects.all()
+        
+    elif request.user.role == User.USER:
+        books = Book.objects.filter(is_borrowed=False)
+    
+    return HttpResponse(serializers.serialize('json', books))
