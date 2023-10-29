e88b24c2b0647a01469b791286f250065811771f
narendradzulqarnain <narendra.dz1107@gmail.com>
2023-10-29 14:36:40 +0700

    fixed css bug

--- InkWanderers/urls.py
+++ InkWanderers/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('api/books/', include(('book.urls'))),
     path('account/', include('account.urls')),
     path('catalogue/', include('main.urls')),
-
+    path('reviews/', include('reviews.urls')),
 ]
