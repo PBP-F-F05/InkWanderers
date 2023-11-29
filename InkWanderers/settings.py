5cf1a8cce5ab687e189dad7418329aced447295d
murifq <muhamad.rifqi510@gmail.com>
2023-11-29 23:26:07 +0700

    maaf lupa trusted origins

--- InkWanderers/settings.py
+++ InkWanderers/settings.py
@@ -165,5 +165,5 @@ SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SAMESITE = 'None'
 SESSION_COOKIE_SAMESITE = 'None'
 
-# CSRF_TRUSTED_ORIGINS =["http://127.0.0.1:8000/"]
+CSRF_TRUSTED_ORIGINS =["https://inkwanderers-f05-tk.pbp.cs.ui.ac.id"]
 
