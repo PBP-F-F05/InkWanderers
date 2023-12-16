5acec2f6d0c55fe279e87b87614e176ac7f9c79b
narendradzulqarnain <narendra.dz1107@gmail.com>
2023-12-16 23:48:18 +0700

    progress review

--- reviews/views.py
+++ reviews/views.py
@@ -1,8 +1,9 @@
+import json
 from django.shortcuts import render
 from reviews.models import Review, ReviewSerializer
 from book.models import Book
 # Create your views here.
-from django.http import HttpResponseRedirect, HttpResponseNotFound
+from django.http import HttpResponseRedirect, HttpResponseNotFound, JsonResponse
 from reviews.forms import ProductForm
 from django.urls import reverse
 from django.http import HttpResponse
@@ -10,7 +11,7 @@ from django.core import serializers
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 from collection.models import CollectionBook, Collection
-from account.models import Profile
+from account.models import BookSerializer, Profile
 @login_required
 def show_review(request, id):
     book = Book.objects.get(pk=id)
@@ -64,13 +65,17 @@ def show_json_by_id(request, id):
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
-
+from django.core.serializers import get_serializer
+def show_json_flutter(request):
+    data= Review.objects.all()
+    serializer = ReviewSerializer(data, many=True)
+    return HttpResponse(serializer.data, content_type="application/json")
 @api_view(('GET',))
+@csrf_exempt
 def get_review_json(request):
     data = Review.objects.filter(user=request.user)
     serializer = ReviewSerializer(data, many=True)
-    return Response(data=serializer.data)
+    return JsonResponse(data=serializer.data, safe=False)
 
 @csrf_exempt
 def edit_review_ajax(request, id):
@@ -87,3 +92,63 @@ def edit_review_ajax(request, id):
         book.save()
         return HttpResponse(status=200)
     return HttpResponseNotFound()
+
+@login_required
+@csrf_exempt
+def add_review_flutter(request, id):
+    if request.method == 'POST':
+        
+        # profile = Profile.objects.filter(user=request.user)[0]
+        # collection = Collection.objects.filter(owner=profile)[0]
+        data = json.loads(request.body)
+        # id2 = int(data['pk'])
+        book = Book.objects.get(pk = id)
+        # collection = Collection.objects.filter(owner = profile)[0]
+        # collection_book = CollectionBook.objects.filter(book = book)[0]
+        # collection_book.book.is_borrowed = False
+        new_product = Review.objects.create(
+            user = request.user,
+            book = book,
+            rating = int(data["rating"]),
+            review = data["review"]
+        )
+        new_product.save()
+        # collection_book.book.review_count+=1
+        # collection_book.book.review_points+= new_product.rating
+        # collection_book.book.save()
+        # collection_book.delete()
+        # collection.save()
+        book.review_points += int(data["rating"])
+        book.review_count += 1
+        book.save()
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
+
+def show_review_by_book(request, id):
+    book = Book.objects.get(pk=id)
+    data = Review.objects.filter(book = book)
+    serializer = ReviewSerializer(data, many=True)
+    return JsonResponse(data=serializer.data, safe=False)
+
+def get_book_json_id(request, id):
+    data = Book.objects.get(pk=id)
+    return HttpResponse(serializers.serialize("json", [data]), content_type="application/json")
+
+@csrf_exempt
+def edit_review_flutter(request, id):
+    if request.method == "POST":
+        data = json.loads(request.body)
+        review = Review.objects.get(pk=id)
+        book = Book.objects.get(pk=review.book.pk)
+        rating = data["rating"]
+        reviewText = data["review"]
+        book.review_points -= review.rating
+        book.review_points += int(rating)
+        review.rating = int(rating)
+        review.review = reviewText
+        review.save()
+        book.save()
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
